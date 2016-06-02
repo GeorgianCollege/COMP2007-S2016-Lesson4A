5187aaa10cc334f8f1cf1d58f48886ee2229c14b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-06-02 12:02:29 -0400

    added EF Model, Student and StudentDetails pages

---
+++ COMP2007-Lesson4A/StudentDetails.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// connect to EF db
+using COMP2007_Lesson4A.Models;
+using System.Web.ModelBinding;
+
+namespace COMP2007_Lesson4A
+{
+    public partial class StudentDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Students.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Use EF to connect to the Server
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // use the student model to save a new record
+                Student newStudent = new Student();
+
+                newStudent.LastName = LastNameTextBox.Text;
+                newStudent.FirstMidName = FirstNameTextBox.Text;
+                newStudent.EnrollmentDate = Convert.ToDateTime(EnrollmentDateTextBox.Text);
+
+                // adding the new student to the collection
+                db.Students.Add(newStudent);
+
+                // run an insert command
+                db.SaveChanges();
+
+                // redirect back to the updated students page
+                Response.Redirect("~/Students.aspx");
+            }
+        }
+    }
+}
--- COMP2007-Lesson4A/Students.aspx.cs
+++ COMP2007-Lesson4A/Students.aspx.cs
@@ -5,13 +5,38 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for connected to EF db
+using COMP2007_Lesson4A.Models;
+using System.Web.ModelBinding;
+
 namespace COMP2007_Lesson4A
 {
     public partial class Students : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // if loading the page for the first time
+            if(!IsPostBack)
+            {
+                // Get Students from EF db
+                this.GetStudents();
+            }
+        }
 
+
+        protected void GetStudents()
+        {
+            // connect to EF
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // query the Students table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+
+                // bind the result to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
         }
     }
 }
